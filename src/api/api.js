91f902249e642bc4a164d620a7c913111b806376
saizakapps <saibalaji315@gmail.com>
2025-03-18 23:52:19 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://real-estate-website-backend-sjr7.onrender.com";
+const API_URL = "https://real-estate-website-backend-sjr7.onrender.com/api";
 
 export const registerUser = async (userData) => {
     return await axios.post(`${API_URL}/auth/register`, userData);
